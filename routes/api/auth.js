b29320a219f43676a75809e05d5453e8a3372952
Sergey Shmatok <trycifer@gmail.com>
2023-07-21 20:37:50 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const { auth } = require('../../middlewares/index');
+const { auth } = require("../../middlewares/index");
 
-const { auth: ctrl } = require('../../controllers/index');
+const { auth: ctrl } = require("../../controllers/index");
 
-router.post('/register', ctrl.register);
+router.post("/register", ctrl.register);
 
-router.post('/login', ctrl.login);
+router.post("/login", ctrl.login);
 
-router.get('/current', auth, ctrl.getCurrent);
+router.get("/current", auth, ctrl.getCurrent);
 
-router.get('/logout', auth, ctrl.logout);
+router.post("/logout", auth, ctrl.logout);
 
-router.patch('/', auth, ctrl.updateSubscriptionUser);
+router.patch("/", auth, ctrl.updateSubscriptionUser);
 
 module.exports = router;
