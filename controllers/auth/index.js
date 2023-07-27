2769cb9601792c0d003e153926013d80373bb11c
Sergey Shmatok <trycifer@gmail.com>
2023-07-27 21:44:45 +0300

    Fixed bug

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,8 +1,9 @@
-const register = require('./register');
-const login = require('./login');
-const getCurrent = require('./getCurrent');
-const logout = require('./logout');
-const updateSubscriptionUser = require('./updateSubscriptionUser');
+const register = require("./register");
+const login = require("./login");
+const getCurrent = require("./getCurrent");
+const logout = require("./logout");
+const updateSubscriptionUser = require("./updateSubscriptionUser");
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   register,
@@ -10,4 +11,5 @@ module.exports = {
   getCurrent,
   logout,
   updateSubscriptionUser,
+  updateAvatar,
 };
