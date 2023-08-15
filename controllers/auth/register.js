512ac9a3cbc03927a9d1ea90eba9d13977983652
Sergey Shmatok <trycifer@gmail.com>
2023-08-15 14:52:05 +0300

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -35,7 +35,7 @@ const register = async (req, res, next) => {
     console.log(result);
 
     const emailTransport = nodemailer.createTransport({
-      service: "gmail",
+      service: "hotmail",
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
